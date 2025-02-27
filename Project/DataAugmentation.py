5f0710cd0daaada6d5a8d0655db11e5c469b9cf6
Murerwa Daniel <murerwadaniel1@gmail.com>
2025-02-27 23:26:19 +0300

    Completed Preparation stage
    27TH FEB WORKED ON DATA PREPARATION. This stage I segmente audio file by taking every sample and adding crackle and wheezle to each audio. I also encoded the information of wheezle and crackle to the audio file. Lastly, each file has a segment name where the number indicate time that instance happen. The metadata for this was provided in the annotation files of every audio data

---
+++ Project/DataAugmentation.py
@@ -0,0 +1,73 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+import shutil
+
+# Function to extract audio features
+patient_diagnosis_info = pd.read_csv(r'C:\Users\Jiary\Documents\GitHub\ML\patient_diagnosis_filtered_data.csv',
+    names=['pid', 'disease'])
+print(patient_diagnosis_info.head())
+audio_dir = r'C:\Users\Jiary\Documents\GitHub\ML\Filtered_audio_files'
+print(len(os.listdir(audio_dir)))
+output_dir=r'C:\Users\Jiary\Documents\GitHub\ML\Augmented_Audio_Files'
+os.makedirs(output_audio_dir, exist_ok=True)
+
+files = [s.split('.')[0] for s in os.listdir(audio_dir) if '.txt' in s]
+
+def getFilenameInfo(file):
+    return file.split('_')
+
+files_data = []
+for file in files:
+    data = pd.read_csv(audio_dir + file + '.txt', sep='\t', names=['start', 'end', 'crackles', 'wheezles'])
+    name_data = getFilenameInfo(file)
+    data['pid'] = name_data[0]
+    data['mode'] = name_data[-2]
+    data['filename'] = file
+    files_data.append(data)
+
+files_df = pd.concat(files_data)
+files_df.reset_index(inplace=True)
+patient_diagnosis_info['pid'] = patient_diagnosis_info['pid'].astype('int32')
+files_df['pid'] = files_df['pid'].astype('int32')
+data = pd.merge(files_df, patient_diagnosis_info, on='pid')
+
+target_samples = 160
+filtered_data = []
+for disease in data['disease'].unique():
+    disease_data = data[data['disease'] == disease]
+    if len(disease_data) >= target_samples:
+        filtered_data.append(disease_data.head(target_samples))  
+
+final_data = pd.concat(filtered_data)
+
+os.makedirs('csv_data', exist_ok=True)
+final_data.to_csv('csv_data/data_filtered.csv', index=False)
+
+
+
+# Create directories for audio and text files
+"""
+audio_files = os.path.join(audio_dir, 'audio_files')
+text_files = os.path.join(audio_dir, 'text_files')
+
+os.makedirs(audio_files, exist_ok=True)
+os.makedirs(text_files, exist_ok=True)
+
+# Sort files into respective directories
+for filename in os.listdir(audio_dir):
+    if filename.endswith('.wav'):
+        shutil.move(os.path.join(audio_dir, filename), os.path.join(audio_files, filename))
+        
+    elif filename.endswith('.txt'):
+        shutil.move(os.path.join(audio_dir, filename), os.path.join(text_files, filename))
+        
+
+print("Files have been sorted!")
+
+print(len(os.listdir(audio_files)))
+print(len(os.listdir(text_files)))
+"""
--- Project/DataPreprocessing.py
+++ Project/DataPreprocessing.py
@@ -1,12 +1,7 @@
 import os
-import random
 import shutil
-import tensorflow as tf
 import pandas as pd
 import numpy as np
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
 
 # Load patient diagnosis information
 patient_diagnosis_info = pd.read_csv(r'C:\Users\Jiary\Downloads\archive\Respiratory_Sound_Database\Respiratory_Sound_Database\patient_diagnosis.csv',
@@ -29,27 +24,30 @@ print(f"Found {len(wav_files)} audio files and {len(txt_files)} annotation files
 #dropping asthma with a single case
 input_file=patient_diagnosis_info
 asthma_rows=input_file[input_file['disease']=='Asthma']
-df_filter_asthma=input_file[input_file['disease']!='Asthma']
+LRTI_rows=input_file[input_file['disease']=='LRTI']
+df_filtered=input_file[(input_file['disease']!='Asthma') & (input_file['disease']!='LRTI')]
 output_file_path=os.path.join(output_dir,'patient_diagnosis_filtered_data.csv')
-df_filter_asthma.to_csv(output_file_path,index=False)
+df_filtered.to_csv(output_file_path, index=False)
 print("Asthma rows dropped. Number of rows dropped: ", len(asthma_rows))
+print("Number of LRTI rows dropped: ", len(LRTI_rows))
 print("Filtered dataset saved to: ", output_dir) 
 
 #dropping audio and text files for asthma
 asthma_pids= set(patient_diagnosis_info[patient_diagnosis_info['disease']=='Asthma']['pid'].astype(str))
+LRTI_pids= set(patient_diagnosis_info[patient_diagnosis_info['disease']=='LRTI']['pid'].astype(str))
+pids_to_remove=asthma_pids.union(LRTI_pids)
+print("Total number of patients to remove: ", len(pids_to_remove))
 
-def is_asthma_file(filename, asthma_pids):
+def is_file_to_remove(filename, pid_to_remove):
     patient_id=filename.split('_')[0]
-    return patient_id in asthma_pids
-wav_files = sorted([f for f in os.listdir(audio_dir) if f.endswith(".wav")])
-txt_files = sorted([f for f in os.listdir(audio_dir) if f.endswith(".txt")])
-      
+    return patient_id in pid_to_remove
+    
 for file in wav_files + txt_files:
-    if not is_asthma_file(file, asthma_pids):
+    if not is_file_to_remove(file, pids_to_remove):
         src_path=os.path.join(audio_dir, file)
         dst_path=os.path.join(output_audio_dir, file)
         shutil.copy(src_path, dst_path)
 
     else:
-        print(f"File {file} is from an asthma patient. Skipping.")
+        print(f"File {file} is from an asthma or LRTI patient. Skipping.")
 print("Filtered audio files saved to: ", output_audio_dir)
